5b53fb4c69fb39eb201f341491703a8a4b63d27d
specter01wj <specter01wj@gmail.com>
2017-11-14 18:00:57 -0500

    11/14/2017 v1

---
+++ dep_guide/5.Scopes/test_9.js
@@ -0,0 +1,13 @@
+(function(){
+  
+  var app = angular.module('scopeExample', []);
+
+  app.controller('MyController', ['$scope', function($scope) {
+    $scope.username = 'World';
+
+    $scope.sayHello = function() {
+      $scope.greeting = 'Hello ' + $scope.username + '!';
+    };
+  }]);
+
+})();
