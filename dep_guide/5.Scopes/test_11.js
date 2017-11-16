ae4062e5671c1324d6bbbebcb7957f810a427070
specter01wj <specter01wj@gmail.com>
2017-11-16 17:54:19 -0500

    11/16/2017 v1

--- dep_guide/5.Scopes/test_11.js
+++ dep_guide/5.Scopes/test_11.js
@@ -3,10 +3,13 @@
   	var app = angular.module('eventExample', []);
 
   	app.controller('EventController', ['$scope', function($scope) {
+
 	  	$scope.count = 0;
+	  	
 	  	$scope.$on('MyEvent', function() {
 	    	$scope.count++;
 	  	});
+
 	}]);
 
 })();
