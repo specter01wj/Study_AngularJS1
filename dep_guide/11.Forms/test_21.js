0aaa0f9986ba8d9e1f484ca7f7f774229ea4f9f8
specter01wj <specter01wj@gmail.com>
2018-01-17 17:41:00 -0500

    01/17/2018 v1

---
+++ dep_guide/11.Forms/test_21.js
@@ -0,0 +1,19 @@
+(function(){
+
+  	var app = angular.module('formExample', []);
+
+  	app.controller('ExampleController', ['$scope', function($scope) {
+      $scope.master = {};
+
+      $scope.update = function(user) {
+        $scope.master = angular.copy(user);
+      };
+
+      $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+      };
+
+      $scope.reset();
+    }]);
+
+})();
