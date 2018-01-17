390fb7071d0265d3690e87f8d9f2ad774633d0f8
specter01wj <specter01wj@gmail.com>
2018-01-17 18:10:53 -0500

    01/17/2018 v3

---
+++ dep_guide/11.Forms/test_23.js
@@ -0,0 +1,9 @@
+(function(){
+
+  	var app = angular.module('customTriggerExample', []);
+
+  	app.controller('ExampleController', ['$scope', function($scope) {
+      $scope.user = {};
+    }]);
+
+})();
