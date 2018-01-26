021f2b40c82e368f4c5d6871e35f14597a4cbcf0
specter01wj <specter01wj@gmail.com>
2018-01-26 17:38:26 -0500

    01/26/2018 v1

---
+++ dep_guide/12.Directives/test_29.js
@@ -0,0 +1,18 @@
+(function(){
+
+  	var app = angular.module('docsSimpleDirective', []);
+
+	app.controller('Controller', ['$scope', function($scope) {
+	    $scope.customer = {
+	      name: 'Naomi',
+	      address: '1600 Amphitheatre'
+	    };
+	}]);
+
+	app.directive('myCustomer', function() {
+	    return {
+	      template: 'Name: {{customer.name}} <br>Address: {{customer.address}}'
+	    };
+	});
+
+})();
