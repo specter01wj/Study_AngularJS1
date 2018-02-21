9fe89255506cba1519e90495482fd7260ef13565
specter01wj <specter01wj@gmail.com>
2018-02-21 18:21:23 -0500

    02/21/2018 v1

---
+++ dep_guide/12.Directives/test_31.js
@@ -0,0 +1,26 @@
+(function(angular) {
+
+  'use strict';
+
+angular.module('docsTemplateUrlDirective', [])
+  .controller('Controller', ['$scope', function($scope) {
+    $scope.customer = {
+      name: 'Naomi',
+      address: '1600 Amphitheatre'
+    };
+  }])
+  .directive('myCustomer', function() {
+    return {
+      restrict: 'AEC',
+      templateUrl: function(elem, attr) {
+        return 'customer-' + attr.jin + '.html';
+      }
+    };
+  });
+})(window.angular);
+
+/*
+Copyright 2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
