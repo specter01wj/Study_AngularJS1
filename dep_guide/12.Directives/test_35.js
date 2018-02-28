240ab35fc4bceeda4e10f18aaa464bbfdd4e2a8d
specter01wj <specter01wj@gmail.com>
2018-02-27 19:08:30 -0500

    02/27/2018 v1

---
+++ dep_guide/12.Directives/test_35.js
@@ -0,0 +1,23 @@
+(function(angular) {
+  'use strict';
+angular.module('docsIsolationExample', [])
+  .controller('Controller', ['$scope', function($scope) {
+    $scope.naomi = { name: 'Naomi', address: '1600 Amphitheatre' };
+    $scope.vojta = { name: 'Vojta', address: '3456 Somewhere Else' };
+  }])
+  .directive('myCustomer', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        customerInfo: '=info'
+      },
+      templateUrl: 'my-customer-plus-vojta.html'
+    };
+  });
+})(window.angular);
+
+/*
+Copyright 2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
