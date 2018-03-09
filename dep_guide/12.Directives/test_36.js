c6477b97814ab21ef5da3d5a1fb212016e045762
specter01wj <specter01wj@gmail.com>
2018-03-09 18:16:39 -0500

    03/09/2018 v1

---
+++ dep_guide/12.Directives/test_36.js
@@ -0,0 +1,43 @@
+(function(angular) {
+  'use strict';
+angular.module('docsTimeDirective', [])
+  .controller('Controller', ['$scope', function($scope) {
+    $scope.format = 'M/d/yy h:mm:ss a';
+  }])
+  .directive('myCurrentTime', ['$interval', 'dateFilter', function($interval, dateFilter) {
+
+    function link(scope, element, attrs, controller, transcludeFn) {
+      var format,
+          timeoutId;
+      console.log('DDD');
+
+      function updateTime() {
+        element.text(dateFilter(new Date(), format));
+      }
+
+      scope.$watch(attrs.myCurrentTime, function(value) {
+        format = value;
+        updateTime();
+      });
+
+      element.on('$destroy', function() {
+        $interval.cancel(timeoutId);
+      });
+
+      // start the UI update process; save the timeoutId for canceling
+      timeoutId = $interval(function() {
+        updateTime(); // update DOM
+      }, 10000);
+    }
+
+    return {
+      link: link
+    };
+  }]);
+})(window.angular);
+
+/*
+Copyright 2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
